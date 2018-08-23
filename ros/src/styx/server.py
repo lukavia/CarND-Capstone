c5d28b10242dd9c0bcdc4bd41323990bc220c28b
Anton Avramov <lukav@lukav.com>
2018-08-23 18:46:06 -0400

    Try to fight latency as per https://github.com/jdleesmiller/CarND-Capstone/commit/33dae9248a73feab9b577dd135116b6575e85788

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -13,7 +13,10 @@ from conf import conf
 
 sio = socketio.Server()
 app = Flask(__name__)
-msgs = []
+
+# Changed to only send the latest message for each topic, rather than queuing out of date messages.
+# Based on https://github.com/jdleesmiller/CarND-Capstone/commit/33dae9248a73feab9b577dd135116b6575e85788
+msgs = {}
 
 dbw_enable = False
 
@@ -22,9 +25,7 @@ def connect(sid, environ):
     print("connect ", sid)
 
 def send(topic, data):
-    s = 1
-    msgs.append((topic, data))
-    #sio.emit(topic, data=json.dumps(data), skip_sid=True)
+    msgs[topic] = data
 
 bridge = Bridge(conf, send)
 
@@ -36,7 +37,7 @@ def telemetry(sid, data):
         bridge.publish_dbw_status(dbw_enable)
     bridge.publish_odometry(data)
     for i in range(len(msgs)):
-        topic, data = msgs.pop(0)
+        topic, data = msgs.popitem()
         sio.emit(topic, data=data, skip_sid=True)
 
 @sio.on('control')
